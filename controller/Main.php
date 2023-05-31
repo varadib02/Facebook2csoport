3e64587b9bec7efc8162f414a82caa10704140c5
Tóth Milán <113629680+tothm23@users.noreply.github.com>
2023-05-31 10:47:27 +0200

    Kész

---
+++ controller/Main.php
@@ -0,0 +1,17 @@
+<?php 
+
+//require_once('../modell/User.php');
+require_once('../service/PublicAPI.php');
+
+//header('content-type: application/json');
+//$user = new User('alma@korte.hu', '1234', 'JakAb', 'gipsz', 33);
+
+//print 'A user neve: ' . $user->getName() . '<br />';
+//print 'A user joga: ' . $user->getLevel() . '<br />';
+// print '{"name": "' . $user->getName() . '", "level": "' . $user->getLevel() . '"}';
+
+
+$data = getAPIData();
+$file = 'api_data.json';
+file_put_contents($file, $data);
+?>
---
+++ service/PublicAPI.php
@@ -0,0 +1,45 @@
+<?php 
+
+/*
+header('content-type: application/json');
+
+$langT = array('en', 'de');
+$lang = $langT[rand(0, 1)];
+
+$url = 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=' . $lang;
+
+$result = file_get_contents($url);
+echo $result;
+*/ 
+
+/*
+header('content-type: application/json');
+
+$count = 10;
+
+$url = 'https://meowfacts.herokuapp.com/?count=' . $count;
+
+$result = file_get_contents($url);
+echo $result;
+*/
+
+/*
+header('content-type: application/json');
+
+$url = 'https://arbeitnow.com/api/job-board-api';
+$result = file_get_contents($url);
+
+echo $result;
+*/
+
+function getAPIData() {
+    // Előző kérés Service rétegből GET az API-ra
+  
+    // Például, itt hívhatod meg az API-t és elmentheted a választ egy változóba
+    $apiResponse = file_get_contents('https://arbeitnow.com/api/job-board-api');
+    // Kód az API hívásához és válasz elmentéséhez
+    
+    // Visszatérési érték JSON formátumban
+    return json_encode($apiResponse);
+}
+?>
